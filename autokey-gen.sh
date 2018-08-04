8e99bf7e3e35e1c62f7caa0ab09de1cfbb67815c
Nam Giang <kyng@ece.ubc.ca>
2018-08-03 19:33:08 -0700

    init

---
+++ autokey-gen.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ctrlkeys=("a" "b" "c" "d" "f" "h" "n" "p" "q" "r" "s" "t" "v" "x" "z")
+
+for key in "${ctrlkeys[@]}"
+do
+  echo "mapping for ctrl-$key to alt-$key"
+  echo "keyboard.send_keys('<ctrl>+$key')" > alt-$key.py
+cat >.alt-$key.json <<kyng
+{
+    "type": "script",
+    "description": "alt-$key",
+    "store": {},
+    "modes": [
+        3
+    ],
+    "usageCount": 0,
+    "prompt": false,
+    "omitTrigger": false,
+    "showInTrayMenu": false,
+    "abbreviation": {
+        "abbreviations": [],
+        "backspace": true,
+        "ignoreCase": false,
+        "immediate": false,
+        "triggerInside": false,
+        "wordChars": "[\\\w]"
+    },
+    "hotkey": {
+        "modifiers": [
+            "<alt>"
+        ],
+        "hotKey": "$key"
+    },
+    "filter": {
+        "regex": null,
+        "isRecursive": false
+    }
+}
+kyng
+done
